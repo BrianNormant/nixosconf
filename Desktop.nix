d499acb9d27a7cb66c9e6e4c2c0f490b435b1172
BrianNixDesktop <briannormant@gmail.com>
2024-03-24 01:34:16 -0400

    Split the desktop specific change into its own branch

---
+++ Desktop.nix
@@ -0,0 +1,59 @@
+{ pkgs, ... }:
+{
+	boot.kernelPatches = [ {
+		name = "beyondfix";
+		patch = ./beyond.patch;
+	} ];
+
+# For logitech G29
+	hardware.new-lg4ff.enable = true;
+	hardware.usb-modeswitch.enable = true;
+
+
+# Bg running services and daemons
+	services.ollama = {
+		enable = true;
+		acceleration = "rocm";
+	};
+	services.openssh = {
+		enable = true;
+		ports = [ 4269 ];
+		settings.AllowUsers = [ "brian" ];
+		settings.PasswordAuthentication = false;
+	};
+	virtualisation.docker.enable = true;
+
+
+# Games
+	programs.gamescope.enable = true;
+	programs.steam = {
+		enable = true;
+		remotePlay.openFirewall = true;
+		dedicatedServer.openFirewall = true;
+		gamescopeSession.enable = true;
+	};
+	nixpkgs.config.packageOverrides = pkgs: {
+		steam = pkgs.steam.override {
+			extraPkgs = pkgs: with pkgs; [
+				xorg.libXcursor
+				xorg.libXi
+				xorg.libXinerama
+				xorg.libXScrnSaver
+				libpng
+				libpulseaudio
+				libvorbis
+				stdenv.cc.cc.lib
+				libkrb5
+				keyutils
+				libxml2
+				
+				gamemode
+				mangohud
+			];
+		};
+	};
+
+# Special Rules
+	networking.firewall.allowedTCPPorts = [ 4269 ];
+	# networking.firewall.allowedUDPPorts = [];
+}
