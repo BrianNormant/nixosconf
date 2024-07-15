0f373b350285fbb8712671d4adb18f5f63a2843c
BrianNixDesktop <briannormant@gmail.com>
2024-07-14 23:20:19 -0400

    update hardware.graphics because unstable

--- configuration.nix
+++ configuration.nix
@@ -30,14 +30,13 @@
 
 	hardware.graphics = {
 		enable = true;
+		enable32Bit = true;
 		# extraPackages = with pkgs; [
 		# 	libGL
 		# 	vaapiVdpau
 		# 	libva
 		# 	libvdpau-va-gl
 		# ];
-		# driSupport = true;
-		# driSupport32Bit = true;
 		# setLdLibraryPath = true;
 	};
 
