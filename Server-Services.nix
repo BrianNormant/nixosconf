751dd55a1b6981eccd25dc36f175645ae62f637d
BrianNixDesktop <briannormant@gmail.com>
2025-03-28 15:27:51 -0400

    feat(server portfolio): added laravel rest-api with it's database

--- Server-Services.nix
+++ Server-Services.nix
@@ -1,50 +1,117 @@
-{pkgs, inputs, ...}: {
+{pkgs, inputs, config,  ...}: {
 	# To rebuild the server remotly run: ```
 	# nixos-rebuild switch --flake .#BrianNixServer --target-host RootNixServer
 	# ```
 
-	# Webserver
-	security.acme.acceptTerms = true;
-	security.acme.defaults.email = "briannormant@gmail.com";
-	services.httpd = {
+	services.nginx = {
 		enable = true;
-		adminAddr = "briannormant@gmail.com";
-		enablePHP = true;
 		virtualHosts = {
-			"ggkbrian.com" = {
-				documentRoot = "/var/www/main";
+			# Proxy to access ollama and ollama UI
+			"ollama.ggkbrian.com" = {
 				addSSL = true;
 				enableACME = true;
+				locations."/" = {
+					proxyPass = "http://192.168.2.71:11434";
+					recommendedProxySettings = true;
+				};
+			};
+			"chatbot.ggkbrian.com" = {
+				addSSL = true;
+				enableACME = true;
+				locations."/" = {
+					proxyPass = "http://192.168.2.71:43941";
+					recommendedProxySettings = true;
+				};
+			};
+			# Webservers
+			"ggkbrian.com" = {
+				addSSL = true;
+				enableACME = true;
+				locations."/" = {
+					root = "/var/www/main";
+				};
 			};
 			"tch057.ggkbrian.com" = {
-				documentRoot = "/var/www/tch057";
 				addSSL = true;
 				enableACME = true;
+				locations."/" = {
+					root = "/var/www/tch057";
+				};
 			};
 			"portfolio.ggkbrian.com" = {
-				documentRoot = inputs.portfolio.packages."x86_64-linux".default;
 				addSSL = true;
 				enableACME = true;
+				locations = {
+					"/" = {
+						root = inputs.portfolio.packages."x86_64-linux".portfolio-website;
+					};
+					"/api" = {
+						root = inputs.portfolio.packages."x86_64-linux".portfolio-api + /public;
+						# TODO: configure 404 endpoint
+						extraConfig = ''
+							Header always append X-Frame-Options "SAMEORIGIN"
+							Header always set X-XSS-Protection "1; mode=block"
+							Header always set X-Content-Type-Options "nosniff"
+							fastcgi_pass unix:${config.services.phpfpm.pools.portfolio.socket}
+							fastcgi_index index.php
+						'';
+					};
+				};
+			};
+		};
+
+	};
+
+	# PHP
+	services.phpfpm = {
+		pools = {
+			"portfolio" = {
+				user = "nobody";
+				group = "nogroup";
+				phpPackage = pkgs.php.withExtensions ({enabled, all}:
+				enabled ++ [] # example all.imagemagick
+				);
+				settings = {
+					"listen.owner" = config.services.nginx.user;
+					"listen.group" = config.services.nginx.group;
+
+					"pm" = "dynamic";
+					"pm.max_children" = 8;
+					"pm.start_servers" = 2;
+					"pm.min_spare_servers" = 2;
+					"pm.max_spare_servers" = 4;
+					"pm.max_requests" = 500;
+					"request_terminate_timeout" = 300;
+				};
 			};
 		};
 	};
+
+	# Webserver
+	security.acme.acceptTerms = true;
+	security.acme.defaults.email = "briannormant@gmail.com";
 
 	# Local DB
 	services.postgresql =
 		let txt = pkgs.writeTextFile {
 				name = "postgresinit.sql";
+				# maybe use this? https://github.com/NixOS/nixpkgs/pull/326306
 				text = ''
 				ALTER ROLE tch057 WITH PASSWORD '1234';
+				ALTER ROLE portfolio WITH PASSWORD '1234';
 				'';
 				};
 	in {
 		enable = true;
 		enableJIT = true;
-		ensureDatabases = [ "tch057" ];
+		ensureDatabases = [ "tch057" "portfolio" ];
 		ensureUsers = [
 		{
 			name = "tch057";
-			ensureClauses.createdb = true;
+			ensureDBOwnership = true;
+		}
+		{
+			name = "portfolio";
 			ensureDBOwnership = true;
 		}
 		];
