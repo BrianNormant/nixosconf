6d4714327f9f85efe5c1523d8b93a9a0b3eaf3ad
BrianNixDesktop <briannormant@gmail.com>
2025-04-01 22:14:24 -0400

    chore(nixvim): forgor

--- Server-Services.nix
+++ Server-Services.nix
@@ -190,4 +190,4 @@
 		];
 		initialScript = txt;
 	};
-}
+
--- nixvim.lua
+++ nixvim.lua
@@ -1,4 +1,12 @@
-require('gruvbox-material').setup {};
+require('gruvbox-material').setup {
+	contrast = "soft",
+}
+vim.cmd [[
+	hi NormalFloat ctermfg=223 ctermbg=236 guifg=#d4be98 guibg=#32302f
+	hi FloatBorder ctermfg=245 ctermbg=236 guifg=#928374 guibg=#32302f
+	hi FloatTitle  ctermfg=208 ctermbg=236 guifg=#e78a4e guibg=#32302f cterm=bold
+]]
+
 require('nvim-startup').setup {}
 
 
