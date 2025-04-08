95d17cf56f89d0a1b3bf6c4d3f99e2bce78f4df1
BrianNixDesktop <briannormant@gmail.com>
2025-04-07 22:08:05 -0400

    feat(zsh/git): Alias for pretty git log

--- zsh.nix
+++ zsh.nix
@@ -100,9 +100,9 @@
 			glola = "git log --graph --pretty=\"%Cred%h%Creset -%C(auto)%d%Creset %s %Cgreen(%ar) %C(bold blue)<%an>%Creset\" --all";
 			glols = "git log --graph --pretty=\"%Cred%h%Creset -%C(auto)%d%Creset %s %Cgreen(%ar) %C(bold blue)<%an>%Creset\" --stat";
 			glol = "git log --graph --pretty=\"%Cred%h%Creset -%C(auto)%d%Creset %s %Cgreen(%ar) %C(bold blue)<%an>%Creset\"";
-			glo = "git log --oneline --decorate";
-			glog = "git log --oneline --decorate --graph";
-			gloga = "git log --oneline --decorate --graph --all";
+			glo = "git log --all --decorate=short --color --pretty=format:'^%C(dim white)%>(12,trunc)%cr%C(reset)^%C(bold 214)%<(7,trunc)%h%C(reset)' -5 | column -t -s ^";
+			glog = "git log --all --graph --decorate=short --color --pretty=format:'%C(bold 214)%<(7,trunc)%h%C(reset)^%C(dim white)%>(12,trunc)%cr%C(reset)^%C(auto)%>(15,trunc)%D%C(reset)^%C(white)%<(80,trunc)%s%C(reset)' | column -t -s ^";
+			gloga = "git log --all --graph --decorate=short --color --pretty=format:'^%C(dim white)%>(12,trunc)%cr%C(reset)^%C(cyan)%<(10,trunc)%cs%C(reset)^%C(bold 214)%<(7,trunc)%h%C(reset)^%C(auto)%<(15,trunc)%D%C(reset)^%C(white)%s%C(reset)' | column -t -s ^";
 			glp = "_git_log_prettily";
 			glg = "git log --stat";
 			glgp = "git log --stat --patch";
