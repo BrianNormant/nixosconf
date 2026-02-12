017b005a5783b644fce487cab6b70b56efcc8349
BrianNixLaptop <briannormant@gmail.com>
2026-02-11 22:20:35 -0500

    Systemd-boot: Max size

--- common-config/others.nix
+++ common-config/others.nix
@@ -7,6 +7,9 @@
 		defaultLocale = "en_US.UTF-8";
 		defaultCharset = "UTF-8";
 	};
+	boot = {
+		loader.systemd-boot.consoleMode = "max";
+	};
 	# This option defines the first version of NixOS you have installed on this particular machine,
 	# and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
 	#
