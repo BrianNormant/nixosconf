daa7fd913dd486a7853347691c3a9a7e24f507b1
BrianNixDesktop <briannormant@gmail.com>
2026-09-07 17:38:08 -0400

    Replace ollama with llama-cpp

--- server-config/ollama.nix
+++ server-config/ollama.nix
@@ -1,20 +1,33 @@
 {pkgs, main-user, ...}:
 {
-	users.users."${main-user}".extraGroups = [ "ollama" ];
-	services.ollama = {
+	services.llama-cpp = {
 		enable = true;
-		package = pkgs.ollama-rocm;
-		user = "ollama";
-		rocmOverrideGfx = "11.0.0";
-		loadModels = [
-			"qwen3.8:27b"
-			"sweepai/sweep-next-edit"
-			"qwen2.5-coder:3b"
-		];
-		environmentVariables = {
-			"OLLAMA_ORIGINS" = "*";
+		package = pkgs.llama-cpp-vulkan;
+		settings = {
+			host = "127.0.0.1";
+			port = 11434;
+			ctx-size = 16384;
+			models-preset = (pkgs.formats.ini { }).generate "models-preset.ini" {
+				"Qwen3.8" = {
+					hf-repo = "unsloth/Qwen3.8-27B-GGUF";
+					hf-file = "Qwen3.8-27B-UD-Q4_K_M.gguf";
+				};
+				"QwenUncensored" = {
+					hf-repo = "JonathanColetti/Qwen3.8-27B-Uncensored-GGUF";
+					hf-file = "Qwen3.8-27B-Uncensored-noMTP-Q4_K_M.gguf";
+					temp = "0.8";
+				};
+				"QwenCoder" = {
+					hf-repo = "Qwen/Qwen2.5-Coder-1.5B-Instruct-GGUF";
+					hf-file = "qwen2.5-coder-1.5b-instruct-q6_k.gguf";
+				};
+			};
 		};
-		openFirewall = true;
-		host = "127.0.0.1";
+	};
+	systemd.services.llama-cpp = {
+		environment = {
+			XDG_CACHE_HOME = "/var/cache/llama-cpp";
+			MESA_SHADER_CACHE_DIR = "/var/cache/llama-cpp";
+		};
 	};
 }
